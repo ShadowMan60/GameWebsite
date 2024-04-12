a4e1f9314d021f88dcce388b4fdc0ee742c94917
Kevin van Hal <kevinvhal2005@gmail.com>
2024-04-12 23:36:37 +0200

    cubper update
    vanwege neo

--- cubper/cubper.php
+++ cubper/cubper.php
@@ -70,8 +70,8 @@ if (isset($_POST['score'])) {
 </div>
 
 <div id="Description">
-    <H3>Game Description</H3>
-    <p>Vul eigen tekst in</p>
+    <H3>cubper Description</H3>
+    <p>cubper is the best game for people with no live like you.</p>
 </div>
 
 <script>
